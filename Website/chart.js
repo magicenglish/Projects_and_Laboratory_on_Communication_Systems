4f9243b4a365d089e7efc649f99876385d70f0c1
Pietro Inglese <magicenglish93@gmail.com>
2018-07-25 17:47:56 +0200

    add of website

---
+++ Website/chart.js
@@ -0,0 +1,92 @@
+
+(function ($) {
+    // USE STRICT
+    "use strict";
+
+    try {
+        // temperature chart
+        var ctx = document.getElementById("sales-chart");
+        if (ctx) {
+            ctx.height = 150;
+            var myChart = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: ["2010", "2011", "2012", "2013", "2014", "2015", "2016"],
+                    type: 'line',
+                    defaultFontFamily: 'Poppins',
+                    datasets: [{
+                        label: "Temperature",
+                        data: [-40, -20, 0, 10, 30, 50],
+                        backgroundColor: 'transparent',
+                        borderColor: 'rgba(220,53,69,0.75)',
+                        borderWidth: 3,
+                        pointStyle: 'circle',
+                        pointRadius: 5,
+                        pointBorderColor: 'transparent',
+                        pointBackgroundColor: 'rgba(220,53,69,0.75)',
+                    }]
+                },
+                options: {
+                    responsive: true,
+                    tooltips: {
+                        mode: 'index',
+                        titleFontSize: 12,
+                        titleFontColor: '#000',
+                        bodyFontColor: '#000',
+                        backgroundColor: '#fff',
+                        titleFontFamily: 'Poppins',
+                        bodyFontFamily: 'Poppins',
+                        cornerRadius: 3,
+                        intersect: false,
+                    },
+                    legend: {
+                        display: false,
+                        labels: {
+                            usePointStyle: true,
+                            fontFamily: 'Poppins',
+                        },
+                    },
+                    scales: {
+                        xAxes: [{
+                            display: true,
+                            gridLines: {
+                                display: false,
+                                drawBorder: false
+                            },
+                            scaleLabel: {
+                                display: false,
+                                labelString: 'Month'
+                            },
+                            ticks: {
+                                fontFamily: "Poppins"
+                            }
+                        }],
+                        yAxes: [{
+                            display: true,
+                            gridLines: {
+                                display: false,
+                                drawBorder: false
+                            },
+                            scaleLabel: {
+                                display: true,
+                                labelString: 'Value',
+                                fontFamily: "Poppins"
+
+                            },
+                            ticks: {
+                                fontFamily: "Poppins"
+                            }
+                        }]
+                    },
+                    title: {
+                        display: false,
+                        text: 'Normal Legend'
+                    }
+                }
+            });
+        }
+
+
+    } catch (error) {
+        console.log(error);
+    }
